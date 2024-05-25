d16218715c8bad33df76ed69867660cd91d5e8c0
Henrique Silva Ribeiro <henribeirox@gmail.com>
2024-05-25 14:27:53 -0300

    alterações wai-aria parte 2

--- cultura/culturaarte.php
+++ cultura/culturaarte.php
@@ -26,14 +26,14 @@
           <div class="row text-start">
             <b><a href="../cultura/index.php">Home</a> <i class="bi bi-arrow-right"></i> <a href="../cultura/culturaarte.php">Cultura e Arte</a></b>
           </div>
-            <article role="article" aria-labelledby="article-heading">
+            <article role="article" aria-labelledby="article-heading" aria-label="seção da página destacando os principais pontos culturais e artísticos presentes em São Roque, enumerando seis desses monumentos e pontos">
                 <div class="col-12">
                     <h2 id="article-heading">PONTOS CULTURAIS</h2>
                     <div class="row col-12">
                       <section class="col-md-6 monumento_cultural" role="region" aria-labelledby="section-heading">
                         <h3 id="section-heading">Roteiro do Vinho</h3>
                           <figure class="text-center">
-                              <img src="../imgs/cultura/roteirovinho1.webp" alt="uma foto frontal da Vinícola Goes, a principal vinícola de São Roque e uma das primeiras">
+                              <img src="../imgs/cultura/roteirovinho1.webp" alt="uma foto frontal da Vinícola Goes, a principal vinícola de São Roque e uma das primeiras" role="img">
                           </figure>
                           <p>Com produção desde o Século XVII, através do fundador da cidade Pedro Vaz de Barros e de famílias de imigrantes portugueses e italianos, a cultura enóloga de São Roque rapidamente se tornou a sua principal característica, nacionalmente a concedendo o título de ‘Terra do Vinho’, e não é para menos. Registros constam que o cultivo da uva é uma prática vinda desde os primeiros povoados da região, e que foi vista como potencial por Pedro Vaz, o fazendo investir na produção.</p>
                           <p>Hoje em dia, a enocultura de São Roque é reunida quase que inteiramente no Roteiro do Vinho, juntando a Estrada do Vinho, dos Venâncios e a Rodovia Quintino de Lima para formar um passeio agradável ao paladar e para todos. Com cerca de 50 estabelecimentos, o Roteiro do Vinho conta com vinícolas, churrascarias, destilarias, petiscos, restaurantes e muitas outras atrações, com acesso gratuito durante toda a semana - embora nos dias mais cheios, haja cobrança para entrada, proporcionando uma fonte de renda inestimável para a nossa cidade.</p>
@@ -41,12 +41,12 @@
                       <section class= "col-md-6 monumento_cultural" role="region" aria-labelledby="section-heading">
                         <h3 id="section-heading">Brasital</h3>
                         <figure class="text-center">
-                            <img src="../imgs/cultura/brasital.webp" alt="foto tirada da entrada da Brasital, com os prédios e a torre capturados e um céu aberto.">
+                            <img src="../imgs/cultura/brasital.webp" alt="foto tirada da entrada da Brasital, com os prédios e a torre capturados e um céu aberto." role="img">
                         </figure>
                         <p>Construída em meados de 1890 pelo italiano Enrico Dell’Acqua e levando o seu nome até 1919, a Brasital serviu como o local de funcionamento de uma das primeiras indústrias de tecido do país, com a empresa de seu fundador já tendo expandido para a Argentina e a própria Itália. Assim, se tornou um dos prédios históricos mais importantes da região. A Brasital possui uma extensão de 9200 m₂, antes ocupados com galpões produzindo diferentes tipos de tecidos de algodão. Fechada em 1970, o local foi reaberto em 1989, passando a ser um Centro Educacional e Cultural, se tornando um dos maiores símbolos de cultura de São Roque, com biblioteca e oficinas estimulantes à cultura, abertas para a população participar e ingressar cada vez mais nesse ramo.</p>
                         <figure class="text-center">
                           <picture>
-                            <source srcset="../imgs/cultura/brasital2.webp" media="(min-width: 992px)"><img src=".." alt="">
+                            <source srcset="../imgs/cultura/brasital2.webp" media="(min-width: 992px)" role="img"><img src=".." alt="">
                           </picture>
                         </figure>
                       </section>
@@ -55,14 +55,14 @@
                       <section class= "col-md-6 monumento_cultural" role="region" aria-labelledby="section-heading">
                         <h3 id="section-heading">Quilombo do Carmo</h3>
                         <figure class="text-center">
-                            <img src="../imgs/cultura/quilombocarmo2.webp" alt="parte da comunidade do Quilombo do Carmo, com algumas casas e uma árvore">
+                            <img src="../imgs/cultura/quilombocarmo2.webp" alt="parte da comunidade do Quilombo do Carmo, com algumas casas e uma árvore" role="img">
                         </figure>
                         <p>Localizado no Bairro do Carmo, a 25 km do centro, o Quilombo é onde se encontram as ruínas da Senzala e do Casarão que lá existiam, abrigando cerca de 170 famílias em uma área de 6,6 alqueires. Por anos, a comunidade, formada por descendentes de escravos que viveram na região durante o período escravista, vem sendo resistente contra as pressões e avanços urbanos por parte de acordos entre a prefeitura e detentores de terra locais, ainda lutando pela titulação das terras por meio do INCRA e da UFScar, embora haja alguns impasses no momento.</p>
                       </section>
                       <section class="col-md-6 monumento_cultural" role="region" aria-labelledby="section-heading">
                           <h3 id="section-heading">Capela Santo Antônio</h3>
                           <figure class="text-center">
-                              <img src="../imgs/cultura/capelasantoantônio.webp" alt="Entrada da Capela de Santo Antônio, localizada no Sítio Santo Antônio, uma relíquia do passado da cidade na época bandeirante">
+                              <img src="../imgs/cultura/capelasantoantônio.webp" alt="Entrada da Capela de Santo Antônio, localizada no Sítio Santo Antônio, uma relíquia do passado da cidade na época bandeirante" role="img">
                           </figure>
                           <p>Construída em 1681 por Fernão Paes de Barros, o local é um dos Sítios Bandeiristas presentes no país durante o período escravista. Posteriormente, no início do século XX, em uma excursão na companhia de Mario de Andrade, Paulo Duarte notou o estado abandonado e decrépito e, através de uma coletânea de artigos, alertou o povo e o governo para o descaso com o local. Desde então, a Capela vem passando por uma restauração. O Sítio Santo Antônio serve como uma ponte para se entender os conceitos, preconceitos e hábitos da época no qual foi construído.</p>
                       </section>
@@ -71,41 +71,41 @@
                     <section class="col-md-6 monumento_cultural" role="region" aria-labelledby="section-heading">
                           <h3 id="section-heading">Morro do Saboó</h3>
                           <figure class="text-center">
-                              <img src="../imgs/cultura/morrosaboó.webp" alt="visão de longe do Morro do Saboó, um dos principais símbolos naturais da cidade e o ponto mais alto desta também">
+                              <img src="../imgs/cultura/morrosaboó.webp" alt="visão de longe do Morro do Saboó, um dos principais símbolos naturais da cidade e o ponto mais alto desta também" role="img">
                           </figure>
                           <p>O ponto mais alto da cidade, o morro pode ser visto de locais muito afastados do centro, permanecendo como um dos símbolos de São Roque para quem vem de fora. Seu nome significa em tupi-guarani “Morro Pelado”, por conta de sua vegetação rasteira e limpa, que embora com alguns buracos na subida até o topo, oferece uma vista espetacular de toda a cidade e da região ao redor. Desse modo, o Morro do Saboó também se torna um dos pontos mais visitados de São Roque.</p>
                       </section>
                       <section class="col-md-6 monumento_cultural" role="region" aria-labelledby="section-heading">
                           <h3 id="section-heading">Morro do Cruzeiro</h3>
                           <figure class="text-center">
-                              <img src="../imgs/cultura/morrocruzeiro.webp" alt="foto tirada abaixo do monumento no Morro, capturando a cruz e parte da estátua de São Roque enquanto ocorre uma procissão próxima">
+                              <img src="../imgs/cultura/morrocruzeiro.webp" alt="foto tirada abaixo do monumento no Morro, capturando a cruz e parte da estátua de São Roque enquanto ocorre uma procissão próxima" role="img">
                           </figure>
                           <p>Abrigando uma imagem do padroeiro da cidade, o Morro do Cruzeiro é um dos símbolos para os cristãos católicos de São Roque, representando o início das festividades municipais e uma proteção para a cidade como um todo. A novena que acontece no fim de julho atrai centenas de fiéis todos os anos, mantendo viva a chama da festividade religiosa.</p>
                       </section>
                     </div>
                 </div>
             </article>
-            <aside class="row col-12 secao_artistas" role="complementary">
+            <aside class="row col-12 secao_artistas" role="complementary" aria-label="área contendo algumas bandas e grupos artísticos presentes em São Roque. Na seção, há listado três deles">
                     <h2>GRUPOS ARTÍSTICOS</h2>
                     <div class="row grupos">
                       <section class="col-md-6 col-lg-4 grupo" role="region" aria-labelledby="section-heading">
                         <h3 id="section-heading">Coletivo Entre Nós</h3>
                         <figure class="text-center">
-                            <img src="../imgs/cultura/coletivoentrenos.webp" alt="apresentação de uma das produções de Páscoa do Coletivo Entre Nós">
+                            <img src="../imgs/cultura/coletivoentrenos.webp" alt="apresentação de uma das produções de Páscoa do Coletivo Entre Nós, famoso coletivo de teatro de São Roque" role="img">
                         </figure>
                         <p>Sendo um núcleo/subdivisão do Coletivo Narciso – existente desde 2010 -, o Coletivo Entre Nós é focado em abordagens sobre a memória e histórias locais e realizou inúmeras apresentações, tanto em São Roque quanto em outras cidades. Entendendo a necessidade de levar arte e fazer arte para todos, o coletivo trabalha para levar a cultura para todos os cantos.</p>
                       </section>
                       <section class="col-md-6 col-lg-4 grupo" role="region" aria-labelledby="section-heading">
                         <h3 id="section-heading">Companhia de Eros</h3>
                         <figure class="text-center">
-                            <img src="../imgs/cultura/companhiaeros.webp" alt="logotipo oficial da Companhia de Eros">
+                            <img src="../imgs/cultura/companhiaeros.webp" alt="logotipo oficial do grupo artístico Companhia de Eros" role="img">
                         </figure>
                         <p>Criada em 2010, a Companhia de Eros começou na Brasital, e por mais de 10 anos, vem trazendo produções com foco na formação do público para o teatro, com reflexões sobre a sociedade brasileira. Por tamanho tempo de trabalho, o grupo foi reconhecido como Ponto de Cultura a partir da Lei Cultura Viva (13.018/2014) e produz peças teatrais até hoje.</p>
                       </section>
                       <section class="col-lg-4 jamais_vistos" role="region" aria-labelledby="section-heading">
                         <h3 id="section-heading">Banda Jamais Vistos</h3>
                         <figure class="text-center">
-                            <img src="../imgs/cultura/bandajamaisvistos.webp" alt="logotipo da Banda Jamais Vistos">
+                            <img src="../imgs/cultura/bandajamaisvistos.webp" alt="logotipo da Banda Jamais Vistos, com um fundo preto e as letras brancas" role="img">
                         </figure>
                         <p>Focados em levar o espírito do rock brasileiro para as novas e velhas gerações, a Banda Jamais Vistos vem ganhando notoriedade no ramo musical de São Roque e na região, realizando shows em festas e clubes. A banda já tocou nas festas paroquiais, no Clube Come Together e na Neo Química Arena.</p>
                       </section>
