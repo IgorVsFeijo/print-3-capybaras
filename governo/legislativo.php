a581e82f328d9d293765e247723bdf0686b4b5ad
Pietro Ricardo Black Fracalanza Muzy <blackpietro2@gmail.com>
2024-06-23 17:42:17 -0300

    Atribuição do PHP concluida
    COM IMAGENS!

--- governo/legislativo.php
+++ governo/legislativo.php
@@ -31,135 +31,54 @@
         <h2 class="hide">Article - h2</h2>
             <p>A Câmara Municipal é um órgão fundamental na estrutura do poder legislativo em nível municipal. Suas atribuições envolvem a criação, discussão e aprovação de leis que afetam diretamente a vida dos cidadãos locais. Além disso, cabe à Câmara a fiscalização dos atos do Poder Executivo, incluindo a análise e aprovação do orçamento municipal, a autorização de despesas e a apreciação de contas públicas. Outro papel crucial é a representação dos interesses da população, por meio dos vereadores, que devem atuar como porta-vozes dos anseios e demandas dos munícipes, promovendo debates, audiências públicas e propondo soluções para questões pertinentes ao desenvolvimento e bem-estar da comunidade local. Além das atividades legislativas, a Câmara Municipal também é responsável por exercer o papel fiscalizador sobre o Poder Executivo, acompanhando a execução das políticas públicas, o cumprimento das leis e a aplicação dos recursos municipais.</p>
         </article>
-
-    </div>
-    <div class="row">
-        <section class="col-lg-12 col-md-12 col-sm-12">
-            <h1>Poder Legislativo</h1>
-            <h3>PRESIDENTE DA CÂMARA</h3>
-        </section>
-        <aside class="col-lg-6 col-md-6 col-sm-12">
-        <h2 class="hide">Aside - h2</h2>
-            <img src="../imgs/governo/presidcamara.webp" alt="foto do presidente  atual da câmara municipal de São Roque, Rafael Tanzi de Araújo , sentado assinando um papel">
-        </aside>
-
-        <section class="col-lg-6 col-md-6 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <p>Atualmente o presidente da Câmara Municipal de São Roque no ano de 2023 é o vereador: Rafael Tanzi de Araújo. Ao Presidente, representante máximo da Câmara cabem inúmeras atribuições, entre elas a direção das Sessões, os trabalhos legislativos, a presidência e assuntos referentes à Mesa Diretora e relações externas. Ainda lhe cabe: superintender as atividades administrativas, fazer cumprir o Regimento Interno, promulgar as decisões e leis, manter a ordem no recinto da Câmara Municipal e declarar extinto o mandato dos vereadores, do prefeito e do vice-prefeito, quando for o caso.</p>
-        </section>
     </div>
 
-    <div class="row">
-        <section class="col-lg-12 col-md-12 col-sm-12">
-            <h3> VEREADORES </h3>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri1.webp" alt="foto de Antônio José Alves Miranda">
-                <figcaption>Antônio José Alves Miranda</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri2.webp" alt="foto de Claudia Rita Duarte Pedroso">
-                <figcaption>Claudia Rita Duarte Pedroso</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri3.webp" alt="foto de Clovis Antônio Ocuma">
-                <figcaption>Clovis Antônio Ocuma</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri4.webp" alt="foto de Diego gouveia da Costa">
-                <figcaption>Diego gouveia da Costa</figcaption>
-            </figure>
-        </section>
+    <?php
+        $sql = "SELECT p.BiografiaPresidenteLegislativo, p.CargoLegislativo, i.caminhoImagemGoverno, i.CategoriaImagemGoverno, i.descricaoImagemGoverno
+        FROM poderlegislativo p INNER join ImagensGoverno i
+                                on p.CodigoPolitico = i.CodigoPolitico
+        WHERE CargoLegislativo = 'presidente da Câmara'";
+        $resultado = $conecta->query($sql);
+
         
-    </div>
-    <div class="row">
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri5.webp" alt="foto de Guilherme Araujo Nunes">
-                <figcaption>Guilherme Araujo Nunes</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri6.webp" alt="foto de Israel Francisco de Oliveira">
-                <figcaption>Israel Francisco de Oliveira</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri7.webp" alt="foto de José Alexandre Perroni">
-                <figcaption>José Alexandre Perroni</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri8.webp" alt="foto de Julio Antonio Mariano">
-                <figcaption>Julio Antonio Mariano</figcaption>
-            </figure>
-        </section>
-        
-    </div>
-    <div class="row">
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri9.webp" alt="foto de Marcos Roberto Arruda">
-                <figcaption>Marcos Roberto Arruda</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri10.webp" alt="foto de Newton Dias Bastos">
-                <figcaption>Newton Dias Bastos</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri11.webp" alt="foto de Paulo Rogério Noggerini">
-                <figcaption>Paulo Rogério Noggerini</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri12.webp" alt="foto de Rogério Jean da Silva">
-                <figcaption>Rogério Jean da Silva</figcaption>
-            </figure>
-        </section>
-        
-    </div>
-    <div class="row">
-        <section class="col-lg-3 col-md-3 col-sm-12">
-        <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri13.webp" alt="foto de Tiago Vieira Nunes">
-                <figcaption>Tiago Vieira Nunes</figcaption>
-            </figure>
-        </section>
-        <section class="col-lg-3 col-md-3 col-sm-12">
-            <h2 class="hide">Section - h2</h2>
-            <figure>
-                <img src="../imgs/governo/veri14.webp" alt="foto de William da Silva Albuquerque">
-                <figcaption>William da Silva Albuquerque</figcaption>
-            </figure>
-        </section>
+    echo "<div class='row'>";
+    while($linha = $resultado->fetch_assoc()) {
+        echo "<section class='col-lg-12 col-md-12 col-sm-12'>";
+        echo    "<h1>Poder Legislativo</h1>";
+        echo    "<h3>".$linha['CargoLegislativo']."</h3>";
+        echo " </section>";
+        echo "<aside class='col-lg-6 col-md-6 col-sm-12'>";
+        echo "<h2 class='hide'>Aside - h2</h2>";
+        echo '<img src="'.$linha['caminhoImagemGoverno'].'"  alt="'.$linha['descricaoImagemGoverno'].'">';
+        echo "</aside>";
+
+        echo '<section class="col-lg-6 col-md-6 col-sm-12">';
+        echo '<h2 class="hide">Section - h2</h2>';
+        echo "<p>" . $linha["BiografiaPresidenteLegislativo"] . "</p>";
+        echo "</section>";
+    }
+    ?>
+
+    <?php
+        $sql = "SELECT p.CargoLegislativo, i.caminhoImagemGoverno, i.CategoriaImagemGoverno, i.descricaoImagemGoverno, p.NomePolitico
+        FROM poderlegislativo p INNER join ImagensGoverno i
+                                on p.CodigoPolitico = i.CodigoPolitico
+        WHERE CargoLegislativo = 'Vereador';";
+        $resultado = $conecta->query($sql);
+
+        echo '<div class="row">';
+        echo '<section class="col-lg-12 col-md-12 col-sm-12">';
+        echo '<h3> VEREADORES </h3>';
+        echo '</section>';
+        while($linha = $resultado->fetch_assoc()) {
+            echo "<section class='col-lg-3 col-md-3 col-sm-12'>";
+            echo "<figure>";
+            echo '<img src="'.$linha['caminhoImagemGoverno'].'"  alt="'.$linha['descricaoImagemGoverno'].'">';
+            echo "<figcaption>".$linha['NomePolitico']."</figcaption>";
+            echo "</figure>";
+            echo "</section>";
+        }
+    ?>
         <?php 
           include '../includes/inc_referencias.php';
           ?>
