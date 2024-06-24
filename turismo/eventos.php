82782d27450cd60231241a5b5e303411a486f0cc
EmanueleCs <emanucrissilva@gmail.com>
2024-06-24 00:02:57 -0300

    Gastronomia e Eventos com PHP
    Aplicação de Banco de dados

--- turismo/eventos.php
+++ turismo/eventos.php
@@ -43,74 +43,73 @@
           </div>
         </article>
 
-        <aside role="complementary">
-            <h2 class="tur_h2 tur_border">FUTUROS EVENTOS</h2>
-            <div class="row">
-                <section class="col-lg-6 col-md-6 col-sm-12">
-                  <h3 class="hide" aria-hidden="true">h3 - Section</h3>
-                    <figure>
-                        <img src="../imgs/turismo/even_castra_desktop.webp" alt="Imagem com fundo amarelo e roxo com a logo da castra móvel no canto superior esquerdo, no lado esquerdo um cachorro fofo e no meio uma escrita 17 de setembro no recanto da cascata, faça sua inscrição">
-                    </figure>
-                    <p class="text-start">O Castramóvel está a caminho de uma nova parada em São Roque, desta vez na região do Junqueira. No domingo, dia 17 de setembro, o serviço que oferece castração gratuita para cães e gatos estará disponível no Recanto da Cascata, proporcionando acolhimento e cuidado aos pets da população.</p>
-                </section>
+        <?php
+              include '../includes/inc_sql.php';
+              
+              // SQL para selecionar todos os autores
+              $sql = "SELECT * 
+              FROM Eventos E Inner Join ImagensTurismo I
+                                  on E.CodigoEvento = I.CodigoEvento
+                                  WHERE CategoriaEvento = 'Futuro'";
+              $resultado = $conecta->query($sql);
 
-                <section class="col-lg-6 col-md-6 col-sm-12">
-                  <h3 class="hide" aria-hidden="true">h3 - Section</h3>
-                    <figure>
-                        <img src="../imgs/turismo/even_moto_desktop.webp" alt="Imagem com fundo cor vinho com a escrita EXPO MOTO, São roque 2023, 23 e 24 de setembro, recanto da cascata / São roque SP">
-                    </figure>
-                    <p class="text-start" >Um dos encontros mais emblemáticos de motociclistas no estado de São Paulo, estará presente na cidade de São Roque neste fim de semana. Uma história de 20 anos, o evento promete infundir o Recanto da Cascata com toda a energia e paixão pelas duas rodas nos dias 23 e 24 de setembro.</p>
-                </section>
-            </div>
-        </aside>
+              echo "<aside role='complementary'>
+            <h2 class='tur_h2 tur_border'>FUTUROS EVENTOS</h2>";
+              echo "<div class='row'>";
+              while($linha = $resultado->fetch_assoc()) {
+                  
+                  echo "<section class='col-lg-6 col-md-6 col-sm-12'>";
+                  echo "<h3 class='hide' aria-hidden='true'>" . $linha["NomeEvento"]. "</h3>";
+                  echo '<figure><img src="'.$linha['CaminhoImagemTurismo'].'"  alt="'.$linha['DescricaoImagemTurismo'].'"> </figure>';
+                  echo "<p class='text-start'>" . $linha["DescricaoEvento"]. "</p>";
+                  
+                  echo "</section>";
+              }
+
+              echo "</div></article>";
+        ?>
       </main>
 
-        <aside role="complementary">
-            <h2 class="tur_h2 tur_border">EVENTOS ENCERRADOS</h2>
-            <div class="row">
-                <section class="col-lg-6 col-md-6 col-sm-12">
-                  <h3 class="hide" aria-hidden="true">h3 - Section</h3>
-                    <figure>
-                        <img src="../imgs/turismo/even_intime_desktop.webp" alt="Imagem com fundo preto, imagem de duas bandas uma em cada canto superior, no meio o nome do evento Intime Moving, 16 de setembro ás 12 horas, entrada um kilo de alimento">
-                    </figure>
-                    <p class="text-start">No dia 16/09 você já tem compromisso marcado! O primeiro foi incrível, este será sensacional! Convida geral pra este dia, cheio de atrações, comunhão e presença de Deus ! NÃO FIQUE DE FORA</p>
-                </section>
+      <?php         
+              // SQL para selecionar todos os autores
+              $sql = "SELECT * 
+              FROM Eventos E Inner Join ImagensTurismo I 
+                                  on E.CodigoEvento = I.CodigoEvento
+                                  WHERE CategoriaEvento = 'Encerrado' LIMIT 2";
+              $resultado = $conecta->query($sql);
 
-                <section class="col-lg-6 col-md-6 col-sm-12">
-                  <h3 class="hide" aria-hidden="true">h3 - Section</h3>
-                    <figure>
-                        <img src="../imgs/turismo/even_adocao_desktop.webp" alt="Imagem com fundo rosa, um gato e um cachorro no lado direito, no lado esquerdo tem a logo da Feira Permanente, logo abaixo da logo temos as informações Feira de adoção, 29 de outubro, das 9 horas ás 12 horas, no recanto da cascata">
-                    </figure>
-                    <p class="text-start">A ação, organizada pelo Serviço de Zoonoses Municipal, acontecerá das 9h às 12h, e levará ao local cães e gatos que hoje estão sendo cuidados pela prefeitura, para que possam conhecer os visitantes da feira e encontrar um novo lar cheio de carinho e amor.</p>
-                </section>
-            </div>
+              echo "<aside role='complementary'>
+              <h2 class='tur_h2 tur_border'>EVENTOS ENCERRADOS</h2>";
+              echo "<div class='row'>";
+              while($linha = $resultado->fetch_assoc()) {
+                
+                  echo "<section class='col-lg-6 col-md-6 col-sm-12'>";
+                  echo "<h3 class='hide' aria-hidden='true'>" . $linha["NomeEvento"]. "</h3>";
+                  echo '<figure><img src="'.$linha['CaminhoImagemTurismo'].'"  alt="'.$linha['DescricaoImagemTurismo'].'"> </figure>';
+                  echo "<p class='text-start'>" . $linha["DescricaoEvento"]. "</p>";
+                  echo "</section>";
+                
+              }
+              echo "</div>";
 
-            <div class="row">
-                <section class="col-lg-4 col-md-4 col-sm-12">
-                  <h3 class="hide" aria-hidden="true">h3 - Section</h3>
-                    <figure>
-                        <img src="../imgs/turismo/even_japao_desktop.webp" alt="Imagem com fundo branco, no lado direito varias figuras ásiaticas, no lado direito a logo da Mostra Japão, logo abaixo suas informações, entrada gratuita, 07 a 10 de setembro no recanto da cascata">
-                    </figure>
-                    <p class="text-start">Mostra Japão São Roque. De 7 a 10 de setembro.</p>
-                </section>
+              $sql = "SELECT * 
+              FROM Eventos E Inner Join ImagensTurismo I 
+                                  on E.CodigoEvento = I.CodigoEvento
+                                  WHERE CategoriaEvento = 'Encerrado' LIMIT 4 OFFSET 2";
+              $resultado = $conecta->query($sql);
+              echo "<div class='row'>";
+              while($linha = $resultado->fetch_assoc()) {
+                
+                echo "<section class='col-lg-4 col-md-4 col-sm-12'>";
+                echo "<h3 class='hide' aria-hidden='true'>" . $linha["NomeEvento"]. "</h3>";
+                echo '<figure><img src="'.$linha['CaminhoImagemTurismo'].'"  alt="'.$linha['DescricaoImagemTurismo'].'"> </figure>';
+                echo "<p class='text-start'>" . $linha["DescricaoEvento"]. "</p>";
+                echo "</section>";
+              
+            }
+            echo "</div></aside>";
 
-                <section class="col-lg-4 col-md-4 col-sm-12">
-                  <h3 class="hide" aria-hidden="true">h3 - Section</h3>
-                    <figure>
-                        <img src="../imgs/turismo/even_musica_desktop.webp" alt="Imagem com fundo marrom, com duas pessoas a caráter interior no lado direito, no outro lado as seguintes informações: Música de interior, entrada gratuita, 26 de agosto, as 20 horas, na brasital são roque">
-                    </figure>
-                    <p class="text-start">No próximo sábado, 26 de agosto, o Duo Música de Interior apresentará um show especial de música caipira intitulado "Cantando o Interior Paulista" na Brasital.</p>
-                </section>
-
-                <section class="col-lg-4 col-md-4 col-sm-12">
-                  <h3 class="hide" aria-hidden="true">h3 - Section</h3>
-                    <figure>
-                        <img src="../imgs/turismo/even_natal_desktop.webp" alt="Imagem com fundo vermelho e detalhes em verde, ao canto direito inferior temos o bob esponja com roupa de natal, e ao lado esquerdo informações sobre o evento. Natal alegre, personagens e apresentações musicais dia 6 ate 23 de dezembro">
-                    </figure>
-                    <p class="text-start">A ação Natal Alegre, que novamente chega para iluminar diversos pontos da cidade e trazer muitas atrações para toda a população.</p>
-                </section>
-            </div>
-        </aside>
+        ?>
         <?php 
           include '../includes/inc_referencias.php';
           ?>
