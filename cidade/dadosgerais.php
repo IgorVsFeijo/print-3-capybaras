2d70e9558d15ecd4d3a48de159dc96bee9e59ef0
joao-psousa <joao.paulo.mk@gmail.com>
2024-06-27 17:36:25 -0300

    I tried to Get Out
    BUT THEY PUT ME BACK IN

--- cidade/dadosgerais.php
+++ cidade/dadosgerais.php
@@ -21,27 +21,25 @@
 
     <!-- Fim Header -->
 
-    <main id="dados-gerais">
-        <article class="container-lg">
+    <main id="dados-gerais" role="main" aria-label="conteúdo principal do site">
+        <article class="container-lg" role="article" aria-labelledby="article-heading">
 
             <!-- Título do Artigo -->
 
             <div class="poster">
 
                     <hgroup class="titulo-poster-fundo-branco">
-                        <h1 class="h1-cidade
-
-">DADOS GERAIS</h1>
+                        <h1 class="h1-cidade" id="article-heading">DADOS GERAIS</h1>
                         <p class="h3">Informações Úteis</p>
                     </hgroup>
             </div>
 
             <!-- Inicio da sessão de informações em formato Dashboard -->
-            <section class="dashboard-dadosgerais row row-cols-md-2">
-                <h2 class="titulo-sessao vazio">Sessão de Informações</h2>
+            <section class="dashboard-dadosgerais row row-cols-md-2" role="region" aria-labelledby="section-heading">
+                <h2 class="titulo-sessao vazio" id="section-heading">Sessão de Informações</h2>
                 <!-- Informações Gerais -->
-                <section class="dashboard-informacoes-gerais col-md-4">
-                    <h3>Informações Gerais</h3>
+                <section class="dashboard-informacoes-gerais col-md-4" role="region" aria-labelledby="section-heading">
+                    <h3 id="section-heading">Informações Gerais</h3>
                     <ul class="list-unstyled">
                         <li><strong>Tipologia:</strong> Mista (atividade turística, agrícola e vinícola)</li>
                         <li><strong>Gentílico:</strong> São-roquense</li>
@@ -56,18 +54,18 @@
 
                 <div class="flex-grow-1">
                     <div class="row row-cols-md-2">
-                        <section id="aniversario-da-cidade-dashboard">
-                            <h3>Aniversário da Cidade</h3>
+                        <section id="aniversario-da-cidade-dashboard" role="region" aria-labelledby="section-heading">
+                            <h3 id="section-heading">Aniversário da Cidade</h3>
                             <p>16 de Agosto</p>
                         </section>
-                        <section id="clima-dashboard">
-                            <h3>Clima:</h3>
+                        <section id="clima-dashboard" role="region" aria-labelledby="section-heading">
+                            <h3 id="section-heading">Clima:</h3>
                             <p>Temperado</p>
                         </section>
                     </div>
-                    <section id="distancias-dashboard" class="row row-cols-md-2">
+                    <section id="distancias-dashboard" class="row row-cols-md-2" role="region" aria-labelledby="section-heading">
                         <div>
-                            <h3>Distancias</h3>
+                            <h3 id="section-heading">Distancias</h3>
                             <ul class="list-unstyled">
                                 <li>Mairinque – 10</li>
                                 <li>Alumínio – 28</li>
@@ -82,9 +80,9 @@
                         </picture>
                     </section>
                 </div>
-                <section id="dashboard-relevo" class="col-md-8 row row-cols-md-2">
+                <section id="dashboard-relevo" class="col-md-8 row row-cols-md-2" role="region" aria-labelledby="section-heading">
                     <div>
-                        <h3>Relevo</h3>
+                        <h3 id="section-heading">Relevo</h3>
                         <p>Caracterizado por planaltos ricos e vales profundos, com terras férteis que se tornaram o
                             berço da
                             tradição vinícola de São Roque. A cidade é cortada pelo Rio Aracaí e por seus afluentes.</p>
@@ -96,8 +94,8 @@
                                         alt="">
                                 </picture>
                 </section>
-                <section id="bioma-dashboard" class="w-auto flex-grow-1">
-                    <h3>Bioma: Mata Atlantica</h3>
+                <section id="bioma-dashboard" class="w-auto flex-grow-1" role="region" aria-labelledby="section-heading">
+                    <h3 id="section-heading">Bioma: Mata Atlantica</h3>
                                                 <picture>
                                     <source media="(max-width: 576px)" srcset="../imgs/cidade/dados_gerais_sm4.webp">
                                     <source media="(max-width: 768px)" srcset="../imgs/cidade/dados_gerais_md4.webp">
@@ -110,11 +108,11 @@
 
             <!-- Inicio da Secao Enderecos Uteis -->
 
-            <section id="enderecos-uteis">
-                <h2 class="titulo-sessao">ENDEREÇOS ÚTEIS</h2>
+            <section id="enderecos-uteis" role="region" aria-labelledby="section-heading">
+                <h2 class="titulo-sessao" id="section-heading">ENDEREÇOS ÚTEIS</h2>
                 <div class="row row-cols-md-3">
-                    <section>
-                        <h3>SANTA CASA</h3>
+                    <section role="region" aria-labelledby="section-heading">
+                        <h3 id="section-heading">SANTA CASA</h3>
                                                     <picture>
                                     <source media="(max-width: 576px)" srcset="../imgs/cidade/dados_gerais_sm5.webp">
                                     <source media="(max-width: 768px)" srcset="../imgs/cidade/dados_gerais_md5.webp">
@@ -124,8 +122,8 @@
                             class="link-mapa d-block">R. Santa Isabel, 186 - Centro, São Roque - SP, 18130-565
 </a>
                     </section>
-                    <section>
-                        <h3>IGREJA MATRIZ</h3>
+                    <section role="region" aria-labelledby="section-heading">
+                        <h3 id="section-heading">IGREJA MATRIZ</h3>
                                                     <picture>
                                     <source media="(max-width: 576px)" srcset="../imgs/cidade/dados_gerais_sm6.webp">
                                     <source media="(max-width: 768px)" srcset="../imgs/cidade/dados_gerais_md6.webp">
@@ -134,8 +132,8 @@
                                 </picture><a href="https://maps.app.goo.gl/31BZadthwrvU5cRp6"
                             class="link-mapa d-block">Praça da Matriz s/nº Centro, Praça da Matriz, 18 - Centro, São Roque - SP, 18130-330</a>
                     </section>
-                    <section>
-                        <h3>PREFEITURA</h3>
+                    <section role="region" aria-labelledby="section-heading">
+                        <h3 id="section-heading">PREFEITURA</h3>
                                                     <picture>
                                     <source media="(max-width: 576px)" srcset="../imgs/cidade/dados_gerais_sm7.webp">
                                     <source media="(max-width: 768px)" srcset="../imgs/cidade/dados_gerais_md7.webp">
